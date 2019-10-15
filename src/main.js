223c534a929eb8a6a8d43053cee5d7b3e89e27eb
chenjiefeng <chenjiefeng@4399inc.com>
2019-10-15 18:05:05 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueResource from "vue-resource"
+import ElementUI from "element-ui"
+import "element-ui/lib/theme-chalk/index.css"
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+
+Vue.use(VueResource);
+Vue.use(ElementUI);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,90 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row display="margin-top:10px">
+      <el-col :span="4"><el-input v-model="name" placeholder="请输入城市名称"></el-input></el-col>
+      <el-col :span="4"><el-input v-model="code" placeholder="请输入城市代码"></el-input></el-col>
+      <el-button type="primary" @click="addCity(name, code)">新增城市</el-button>
+    </el-row>
+    <el-row>
+      <el-table :data="cityList" style="width: 100%" border>
+        <el-table-column prop="id" label="编号" min-width="100">
+          <template scope="scope">{{ scope.row.pk }}</template>
+        </el-table-column>
+        <el-table-column prop="city_name" label="城市名称" min-width="100">
+          <template scope="scope">{{ scope.row.fields.city_name }}</template>
+        </el-table-column>
+         <el-table-column prop="city_code" label="城市代码" min-width="100">
+          <template scope="scope">{{ scope.row.fields.city_code }}</template>
+        </el-table-column>
+         <el-table-column prop="create_time" label="创建时间" min-width="100">
+          <template scope="scope">{{ scope.row.fields.create_time }}</template>
+        </el-table-column>
+      </el-table>
+    </el-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      name: "",
+      code: 0,
+      cityList: [],
+    }
+  },
+  methods:{
+    fetchList(){
+      this.$http.get("/api/city_list").then(res => {
+        console.log(res);
+          if(res.code == 100){
+              this.$message({
+                type: "success",
+                showClose: true,
+                message: "加载成功",
+              });
+            this.cityList = res.data;
+          }else{
+            this.$message({
+                type: "error",
+                showClose: true,
+                message: "加载失败",
+              });
+          }
+      })
+    },
+    addCity(name, code){
+      let options = {
+        body: {
+          "city_name": name,
+          "city_code": code
+        },
+        emulateJSON: true,
+      };
+      this.$http.get("/api/add_city", [options]).then(res => {
+          console.log(res);
+          if(res.code == 100){
+            this.$message({
+                type: "success",
+                showClose: true,
+                message: "添加成功",
+              });
+            this.fetchList();
+          }else{
+            this.$message({
+                type: "error",
+                showClose: true,
+                message: "添加失败",
+              });
+          }
+      })
+    }
+  },
+  mounted() {
+    this.fetchList();
   }
 }
 </script>
